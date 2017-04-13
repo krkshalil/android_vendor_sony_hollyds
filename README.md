87bf03deee8dd44394aeda545ccc0f8e225ea10e
Halil <krkshalil@outlook.com>
2017-04-13 07:34:30 +0300

    Initial upload

---
+++ README.md
@@ -0,0 +1,6 @@
+# LOS13.0 vendor for Sony Xperia M5 Dual (hollyds)
+
+
+### Credits:
+  - nofearnohappy & LazyC0DEr
+  - Sicmosh
---
+++ hollyds-vendor-blobs.mk
@@ -0,0 +1,15 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/sony/hollyds/app,system/app)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/sony/hollyds/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/sony/hollyds/lib64,system/lib64)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/sony/hollyds/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/sony/hollyds/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/sony/hollyds/usr,system/usr)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/sony/hollyds/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/sony/hollyds/vendor,system/vendor)
